74a76a72a4a71a5b1f20cf9807947c5dff2d8b83
Kazuki Sawada <kazuki@6715.jp>
2023-11-24 00:39:03 +0900

    初期実装

---
+++ src/index.tsx
@@ -0,0 +1,109 @@
+import { SessionService } from "@buf/pocketsign_apis.connectrpc_es/pocketsign/stamp/v1/session_connect";
+import { createPromiseClient } from "@connectrpc/connect";
+import { createConnectTransport } from "@connectrpc/connect-web";
+import { Hono } from "hono";
+import { getCookie, setCookie } from "hono/cookie";
+import { Callback, Index } from "./pages";
+
+const client = createPromiseClient(
+	SessionService,
+	createConnectTransport({
+		baseUrl: "https://verify.mock.p8n.app",
+		useBinaryFormat: true,
+
+		// Cloudflare Workersで動作させるためのWorkaroundです。（通常は不要）
+		fetch: (input, init) =>
+			fetch(input, {
+				method: init?.method,
+				headers: init?.headers,
+				body: init?.body,
+			}),
+	}),
+);
+
+const app = new Hono();
+
+app.get("/", c => {
+	return c.html(<Index />);
+});
+
+app.post("/apply", async c => {
+	const { plan } = await c.req.parseBody();
+
+	const url = new URL(c.req.url);
+	const resp = await client.createSession(
+		{
+			callbackUrl: `${url.origin}/callback`,
+			requests: [
+				{
+					required: true,
+					request: {
+						case: "digitalSignature",
+						value: {
+							content: new TextEncoder().encode(
+								`【申込書】\n利用規約に同意し、選択したプランに申し込みます。\nプラン: ${plan}`,
+							),
+							printableContent: `ポケットサインターネットにお申し込みいただきありがとうございます。申込内容は以下のとおりです。\n\nプラン: ${plan}`,
+						},
+					},
+				},
+				{
+					required: false,
+					request: {
+						case: "personalInfoConsent",
+						value: {
+							preference: {
+								address: true,
+								commonName: true,
+								dateOfBirth: true,
+								gender: true,
+							},
+						},
+					},
+				},
+			],
+		},
+		{
+			headers: {
+				Authorization: `Bearer ${c.env?.POCKETSIGN_TOKEN}`,
+			},
+		},
+	);
+
+	setCookie(c, "session_id", resp.id);
+	return c.redirect(resp.redirectUrl);
+});
+
+app.get("/callback", async c => {
+	const resp = await client.finalizeSession(
+		{
+			id: getCookie(c, "session_id"),
+		},
+		{
+			headers: {
+				Authorization: `Bearer ${c.env?.POCKETSIGN_TOKEN}`,
+			},
+		},
+	);
+
+	const content = resp.results
+		.map(({ result }) => {
+			if (result.case === "digitalSignature") {
+				const content = result.value.result?.certificateContent?.typeSpecificContent;
+				if (content?.case === "jpkiCardDigitalSignatureContent") {
+					return `${content.value.commonName} 様、お申し込みありがとうございました。ご自宅（${content.value.address}）に契約書をお送りします。`;
+				}
+				return `お申し込みが確認できませんでした。`;
+			}
+			if (result.case === "personalInfoConsent") {
+				return `また、最新4情報の提供に同意いただきありがとうございます。同意は ${result.value.result?.expiresAt
+					?.toDate()
+					.toLocaleString()} まで有効です。`;
+			}
+		})
+		.join("\n");
+
+	return c.html(<Callback content={content} />);
+});
+
+export default app;
---
+++ src/pages.tsx
@@ -0,0 +1,43 @@
+import type { FC } from "hono/jsx";
+
+const Layout: FC = ({ children }) => {
+	return (
+		<html lang="ja">
+			<head>
+				<meta charset="utf-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<title>ポケットサインターネット　お申し込みページ</title>
+			</head>
+			<body>
+				<main>{children}</main>
+			</body>
+		</html>
+	);
+};
+
+export const Index: FC = () => {
+	return (
+		<Layout>
+			<h1>ポケットサインターネット　お申し込み</h1>
+			<p>お申し込みはこちらから</p>
+			<form action="/apply" method="post">
+				<select name="plan">
+					<option value="高速プラン">高速プラン</option>
+					<option value="中速プラン">中速プラン</option>
+					<option value="低速プラン">低速プラン</option>
+				</select>
+				<br />
+				<input type="submit" value="申し込む" />
+			</form>
+		</Layout>
+	);
+};
+
+export const Callback: FC<{ content: string }> = ({ content }) => {
+	return (
+		<Layout>
+			<h1>ポケットサインターネット　お申し込み完了</h1>
+			<p>{content}</p>
+		</Layout>
+	);
+};
